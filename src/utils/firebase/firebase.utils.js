e20ad3e389ec97b0be2670fcf307447fc8a900c3
Medeu Amangeldi <medeuamangeldi@Medeus-Mac-mini.local>
2022-05-26 15:33:03 -0500

    added sign in component and firebase utils and firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAtAD1OtY5tbFTgDhG1QhkLJ3T4GKyFdfw",
@@ -10,7 +15,9 @@ const firebaseConfig = {
     messagingSenderId: "915595418212",
     appId: "1:915595418212:web:17242fc8766ac28f031d71"
 };
-  
+
+
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
@@ -22,3 +29,33 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('oops, something went wrong', error.message);
+        }
+    }
+
+    return userDocRef;
+};
